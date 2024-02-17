4c59283a5b290f44fd91a6583853336a4622cac6
Dustin <dustin@undefinedindustries.com>
2024-02-17 12:57:09 -0700

    adding authZero for nodejs app

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const sequelize = require('./util/database');
 const User = require('./models/user');
 
 const app = express();
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,13 +29,19 @@ app.use((req, res, next) => {
     next();
 });
 
-//test route
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Hello World!!' : 'Logged out');
 });
 
 //CRUD Routes
 app.use('/users', require('./routes/users'));
+const { requiresAuth } = require('express-openid-connect');
+
+//protected route example
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 //error handling
 app.use((error, req, res, next) => {
